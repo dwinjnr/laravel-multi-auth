566dbd14943c8d0716c8eb2713935975cda0460e
Godwin Ogbonnaya Jnr <godwinogbonnayajnr@gmail.com>
2018-08-04 06:58:26 +0100

    set up AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin');
+    }
 }
