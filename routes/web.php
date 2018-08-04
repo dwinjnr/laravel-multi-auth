ca6b3d3d19f6277471ecec5695064513517fa98c
Godwin Ogbonnaya Jnr <godwinogbonnayajnr@gmail.com>
2018-08-04 07:11:51 +0100

    set up routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
+});
+
+Route::prefix('admin')->group(function() {
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/dashboard', 'AdminController@index')->name('admin.dashboard');
 });
 
 Auth::routes();
