04d3fa2f502abb745cd28c43c6835673011c31cb
Godwin Ogbonnaya Jnr <godwinogbonnayajnr@gmail.com>
2018-08-04 07:19:28 +0100

    rename instances of home to dashboard, in view- home to user

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,7 +38,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::prefix('admin')->group(function() {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
