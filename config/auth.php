20c38fb46b20d575a2e943e841799f371c017c87
Godwin Ogbonnaya Jnr <godwinogbonnayajnr@gmail.com>
2018-08-04 06:18:41 +0100

    set up config auth

--- config/auth.php
+++ config/auth.php
@@ -45,6 +45,14 @@ return [
             'driver' => 'token',
             'provider' => 'users',
         ],
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
+        'admin-api' => [
+            'driver' => 'token',
+            'provider' => 'admins',
+        ],
     ],
 
     /*
@@ -69,6 +77,11 @@ return [
             'driver' => 'eloquent',
             'model' => App\User::class,
         ],
+        
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Admin::class,
+        ],
 
         // 'users' => [
         //     'driver' => 'database',
@@ -91,11 +104,17 @@ return [
     |
     */
 
+    
     'passwords' => [
         'users' => [
             'provider' => 'users',
             'table' => 'password_resets',
             'expire' => 60,
+        ],
+        'admins' => [
+            'provider' => 'admins',
+            'table' => 'password_resets',
+            'expire' => 15,
         ],
     ],
 
