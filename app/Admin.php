235f1d2e6527c4bbea4d556f8cd7bdbbe1ee2c77
Godwin Ogbonnaya Jnr <godwinogbonnayajnr@gmail.com>
2018-08-04 05:59:07 +0100

    set up admin model

--- app/Admin.php
+++ app/Admin.php
@@ -3,8 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Admin extends Model
 {
-    //
+    use Notifiable;
+
+    // The authentication guard for admin
+    protected $guard = 'admin';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
