d2a6cf70218add535aed10493146c1244414a0fd
Godwin Ogbonnaya Jnr <godwinogbonnayajnr@gmail.com>
2018-08-04 07:55:15 +0100

    fixed an error in admin model

--- app/Admin.php
+++ app/Admin.php
@@ -2,10 +2,10 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
     use Notifiable;
 
